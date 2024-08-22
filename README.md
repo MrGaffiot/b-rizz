35188c3cb249bcb7b567a1bc013419b8d8e69059
MrGaffiot <142170057+MrGaffiot@users.noreply.github.com>
2024-08-22 21:38:55 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# b-rizz
+# Borisova webová stránka
+
+# Pro Borise:
+-pokud cheš uploadnout nový projekt, jdi do sus.com/botistusek
+
+# url_for('static', filename='style.css')
---
+++ boristurek.py
@@ -0,0 +1,44 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return render_template('index.html', render_template=render_template, len=len)
+
+@app.route("/portraits")
+def portraits():
+    return render_template("portraits.html")
+
+@app.route("/test")
+def test():
+    return render_template("test.html")
+
+@app.route("/about")
+def about():
+    return render_template("aboutme.html")
+
+@app.route("/concerts")
+def concerts():
+    return render_template("concerts.html")
+
+@app.route("/nightlibeznice")
+def nightlibeznice():
+    return render_template("nightlibeznice.html")
+
+@app.route("/Sklepos9.9.2023")
+def Sklepos992023():
+    return render_template("Sklepos9.9.2023.html")
+
+"""sumary_line
+@app.route("/project/[JMENO PROJEKTU]")
+def [(JMENO PROJEKTU]):
+    return render_template("JMENO SOUBORU")
+"""
+
+with open("static\\boris.png", "r") as file:
+    print("I <3 Boris")
+    print(file)
+
+app.run()
